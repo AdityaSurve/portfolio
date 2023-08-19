8c53d210745dc6c4aa08a7b1bd93c499fe3fa318
Aditya <surveaditya521@gmail.com>
2023-08-19 22:01:59 +0530

    threejs init

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import ThemeToggler from "./components/ThemeToggler";
-import Skills from "./components/Skills";
-
 const App = () => {
-  const [activeLink, setActiveLink] = useState("Home");
-  const [theme, setTheme] = useState("light");
-  useEffect(() => {
-    console.log(activeLink);
-  }, [activeLink]);
-  return (
-    <div className="font-pops flex flex-col h-screen overflow-hidden justify-center items-center w-screen">
-      <Navbar
-        activeLink={activeLink}
-        setActiveLink={setActiveLink}
-        theme={theme}
-      />
-      <div className="w-full overflow-y-auto">
-        <Banner theme={theme} />
-        <Skills theme={theme} />
-      </div>
-      <ThemeToggler theme={theme} setTheme={setTheme} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
