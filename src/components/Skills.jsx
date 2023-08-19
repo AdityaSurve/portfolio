f0f0a5549f1a716d1ecf95df10dc13ac23489673
Aditya <surveaditya521@gmail.com>
2023-08-19 10:49:33 +0530

    react tooltip for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
-import { skills } from "../data"; // Make sure to adjust the import path
+import { skills } from "../data";
+import { Tooltip } from "react-tooltip";
 
 const Skills = ({ theme }) => {
   return (
-    <div className="flex flex-col h-screen justify-start p-20 items-center">
+    <div className="flex flex-col h-screen justify-center p-20 items-center">
       <div className="text-[#42446E] text-3xl font-bold text-center">
         My Tech Stack
       </div>
@@ -12,17 +13,19 @@ const Skills = ({ theme }) => {
       </div>
       <div className="grid grid-cols-12 mt-5 gap-2 w-full relative overflow-hidden">
         {skills.map((skill, index) => (
-          <div
-            key={skill.name}
-            className={`flex flex-col items-center justify-center gap-2 p-4 rounded-lg transition-all duration-500 ${
-              theme === "light" ? "bg-white" : "bg-[#191919]"
-            }`}
-          >
-            <img
-              src={skill.icon}
-              alt={skill.name}
-              className="h-16 w-16 object-contain"
-            />
+          <div key={skill.name}>
+            <div
+              className="flex items-center justify-center hover:bg-gray-100 rounded-full p-2 transition-all duration-500"
+              data-tooltip-id="skill-name"
+              data-tooltip-content={skill.name}
+            >
+              <img
+                src={skill.icon}
+                alt={skill.name}
+                className="h-16 w-16 object-contain"
+              />
+            </div>
+            <Tooltip id="skill-name" />
           </div>
         ))}
       </div>
