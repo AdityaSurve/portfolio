40bd055175b9edb8dbf995c781cca8dba0dd47d2
Aditya <surveaditya521@gmail.com>
2023-08-20 21:11:16 +0530

    skills final

--- src/components/cards/SkillsCard.jsx
+++ src/components/cards/SkillsCard.jsx
@@ -5,17 +5,18 @@ const SkillsCard = ({ title, description, skills }) => {
     <div className="card-container bg-[#fff1] backdrop-blur-xl cursor-pointer bg-card bg-contain bg-center bg-fixed">
       <div className="card">
         <div className="front-content flex flex-col">
-          <p className="text-xl h-12">{title}</p>
+          <p className="text-xl h-12 bg-gradient-to-t from-[#915EFF10] to-[#915EFF] bg-clip-text text-transparent">
+            {title}
+          </p>
           <p className="text-sm px-6 mt-5 h-16 text-center">{description}</p>
         </div>
         <div className="content">
           <p className="heading h-10">Tech Stack: </p>
           <div className="w-full h-full flex flex-wrap justify-center">
             {skills.map((skill, index) => (
-              <>
+              <div key={index}>
                 <div
-                  key={index}
-                  className="w-14 h-14 p-1 m-2 rounded-full bg-[#fff3] backdrop-blur-2xl flex items-center justify-center"
+                  className="h-10 lg:w-14 w-10 lg:h-14 p-1 m-2 rounded-full bg-[#fff3] backdrop-blur-2xl flex items-center justify-center"
                   style={{
                     zIndex: 1,
                     pointerEvents: "auto",
@@ -37,7 +38,7 @@ const SkillsCard = ({ title, description, skills }) => {
                   delayHide={100}
                   className="z-[1000]"
                 />
-              </>
+              </div>
             ))}
           </div>
         </div>
