531507aea9beb2a0bf7ffa989740b61380cd3f97
Aditya <surveaditya521@gmail.com>
2023-08-21 16:01:53 +0530

    tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { tech } from "../constants";
+import { Tooltip } from "react-tooltip";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="w-full py-10 lg:px-20 gap-10 flex flex-wrap justify-center items-center">
+      {tech.map((t) => (
+        <div
+          className="w-16 h-16 bg-[#ffffff30] rounded-full p-1"
+          data-tooltip-id={t.name}
+          data-tooltip-content={t.name}
+          key={t.id}
+        >
+          <img src={t.icon} alt="" className="w-full h-full" />
+          <Tooltip
+            id={t.name}
+            place="bottom"
+            effect="solid"
+            delayShow={100}
+            delayHide={100}
+            className="z-[1000]"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default Tech;
--- src/constants/index.js
+++ src/constants/index.js
@@ -28,6 +28,14 @@ import deepLearningIcon from "../assets/images/deep-learning.png";
 import nlpIcon from "../assets/images/nlp.png";
 import imageProcessingIcon from "../assets/images/imageProcessing.svg";
 
+import c from "../assets/images/c.svg";
+import cpp from "../assets/images/cpp.svg";
+import java from "../assets/images/java.svg";
+import flutter from "../assets/images/flutter.svg";
+import git from "../assets/images/git.svg";
+import github from "../assets/images/github.svg";
+import kotlin from "../assets/images/kotlin.svg";
+
 import acmegrade from "../assets/acmegrade.png";
 import yhills from "../assets/yhills.png";
 import enpointe from "../assets/enpointe.png";
@@ -216,99 +224,177 @@ const experiences = [
   },
 ];
 
-// const testimonials = [
-//   {
-//     testimonial:
-//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-//     name: "Sara Lee",
-//     designation: "CFO",
-//     company: "Acme Co",
-//     image: "https://randomuser.me/api/portraits/women/4.jpg",
-//   },
-//   {
-//     testimonial:
-//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-//     name: "Chris Brown",
-//     designation: "COO",
-//     company: "DEF Corp",
-//     image: "https://randomuser.me/api/portraits/men/5.jpg",
-//   },
-//   {
-//     testimonial:
-//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-//     name: "Lisa Wang",
-//     designation: "CTO",
-//     company: "456 Enterprises",
-//     image: "https://randomuser.me/api/portraits/women/6.jpg",
-//   },
-// ];
+const tech = [
+  {
+    id: 1,
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    id: 2,
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    icon: js,
+  },
+  {
+    id: 4,
+    name: "React JS",
+    icon: react,
+  },
+  {
+    id: 5,
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    id: 6,
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    id: 7,
+    name: "Material UI",
+    icon: materialUI,
+  },
+  {
+    id: 8,
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    id: 9,
+    name: "SASS",
+    icon: sass,
+  },
+  {
+    id: 10,
+    name: "Next JS",
+    icon: nextjs,
+  },
+  {
+    id: 11,
+    name: "Node.js",
+    icon: nodejs,
+  },
+  {
+    id: 12,
+    name: "Nest JS",
+    icon: nestjs,
+  },
+  {
+    id: 13,
+    name: "Express JS",
+    icon: expressjs,
+  },
+  {
+    id: 14,
+    name: "Flask",
+    icon: flask,
+  },
+  {
+    id: 15,
+    name: "Prisma",
+    icon: prisma,
+  },
+  {
+    id: 16,
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    id: 17,
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    id: 18,
+    name: "Firebase",
+    icon: firebase,
+  },
+  {
+    id: 19,
+    name: "PostgreSQL",
+    icon: postgresql,
+  },
+  {
+    id: 20,
+    name: "Tableau",
+    icon: tableau,
+  },
+  {
+    id: 21,
+    name: "Python",
+    icon: python,
+  },
+  {
+    id: 22,
+    name: "Pandas",
+    icon: pandas,
+  },
+  {
+    id: 23,
+    name: "Artificial Intelligence",
+    icon: aiIcon,
+  },
+  {
+    id: 24,
+    name: "Machine Learning",
+    icon: mlIcon,
+  },
+  {
+    id: 25,
+    name: "Deep Learning",
+    icon: deepLearningIcon,
+  },
+  {
+    id: 26,
+    name: "Natural Language Processing",
+    icon: nlpIcon,
+  },
+  {
+    id: 27,
+    name: "Image Processing and Recognition",
+    icon: imageProcessingIcon,
+  },
+  {
+    id: 28,
+    name: "C",
+    icon: c,
+  },
+  {
+    id: 29,
+    name: "C++",
+    icon: cpp,
+  },
+  {
+    id: 30,
+    name: "Java",
+    icon: java,
+  },
+  {
+    id: 31,
+    name: "Flutter",
+    icon: flutter,
+  },
+  {
+    id: 32,
+    name: "Git",
+    icon: git,
+  },
+  {
+    id: 33,
+    name: "GitHub",
+    icon: github,
+  },
+  {
+    id: 34,
+    name: "Kotlin",
+    icon: kotlin,
+  },
+];
 
-// const projects = [
-//   {
-//     name: "Car Rent",
-//     description:
-//       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-//     tags: [
-//       {
-//         name: "react",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "mongodb",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "tailwind",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: carrent,
-//     source_code_link: "https://github.com/",
-//   },
-//   {
-//     name: "Job IT",
-//     description:
-//       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-//     tags: [
-//       {
-//         name: "react",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "restapi",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "scss",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: jobit,
-//     source_code_link: "https://github.com/",
-//   },
-//   {
-//     name: "Trip Guide",
-//     description:
-//       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-//     tags: [
-//       {
-//         name: "nextjs",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "supabase",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "css",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: tripguide,
-//     source_code_link: "https://github.com/",
-//   },
-// ];
-
-// export { services, technologies, experiences, testimonials, projects };
-
-export { navLinks, skills, experiences };
+export { navLinks, skills, experiences, tech };
